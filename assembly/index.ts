fae5a92cb958deb45030eac64dfbb5b07a32f2d7
Joshua Tenner <tenner.joshua@gmail.com>
2020-01-27 13:01:48 -0500

    [fix] random number generation and add commit() mid loop to prevent buffer overflow

--- assembly/index.ts
+++ assembly/index.ts
@@ -6,17 +6,17 @@ import {
 let ctx: CanvasRenderingContext2D;
 let x: f64 = 0;
 let y: f64 = 0;
-const starCount: i32 = 3000;
+const starCount: i32 = 5000;
 let stars: Float64Array = new Float64Array(starCount * 2);
 export function init(): void {
-  let seed = JSMath.random() * <f64>u64.MAX_VALUE
-  NativeMath.seedRandom(<i64>seed);
+  // let seed1 = <i32>(JSMath.random() * <f64>i32.MAX_VALUE);
+  // let seed2 = <i32>(JSMath.random() * <f64>i32.MAX_VALUE);
+  // NativeMath.seedRandom((<i64>seed1 << 32) | <i64>seed2);
+  NativeMath.seedRandom(reinterpret<i64>(JSMath.random()));
   ctx = getContextById("main");
-  let index: i32;
-  for (let i = 0; i < starCount; i++) {
-    index = i << 1;
-    stars[index] = Math.random() * 800;
-    stars[index + 1] = Math.random() * 600;
+  for (let i = 0; i < starCount; i += 2) {
+    stars[i] = Math.random() * 800;
+    stars[i + 1] = Math.random() * 600;
   }
 }
 
@@ -41,6 +41,7 @@ export function update(): void {
     if (sy > 600.0) sy -= 600.0;
     ctx.fillRect(sx, sy, 2, 2);
     unchecked(stars[index + 1] = sy);
+    if (i % 1000 === 999) ctx.commit();
   }
 
   ctx.beginPath();
